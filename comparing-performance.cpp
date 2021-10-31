e6d6a3f5e81a3f969b4d5ba77879ba9112a9b507
RyanMcPherson7 <ryan7mcpherson@gmail.com>
2021-10-30 23:13:15 -0400

    added standard input tests for comparison

--- comparing-performance.cpp
+++ comparing-performance.cpp
@@ -1219,8 +1219,8 @@ void compareOrderAndUnorder (const int numEntries) {
 int main() {
 
     compareOrderAndUnorder(1000);
-    // compareOrderAndUnorder(10000);
-    // compareOrderAndUnorder(100000);
+    compareOrderAndUnorder(10000);
+    compareOrderAndUnorder(100000);
     // compareOrderAndUnorder(200000);
     // compareOrderAndUnorder(300000);
     // compareOrderAndUnorder(400000);
